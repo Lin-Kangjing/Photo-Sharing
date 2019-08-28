df8062fea4df62c0a57236508741318b84024cc5
lkjngg <linkangjing@foxmail.com>
2019-08-28 19:25:45 +0800

    test

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var mongoose = require('mongoose') 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const photos = require('./routes/photos')
 
 var app = express();
--- routes/photos.js
+++ routes/photos.js
@@ -1,9 +1,6 @@
 // var express = require('express');
 // var router = express.Router();
 const Photo = require('../models/Photo')
-const fs = require('fs')
-const path = require('path')
-const join = path.join
 
 exports.form = (req,res)=>{
   res.render('upload',{
